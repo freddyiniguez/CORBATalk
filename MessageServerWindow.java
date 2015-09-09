facebd6849eac0cc1984df3ced9423f979a22267
Freddy Íñiguez López <iniguez.devt@gmail.com>
2015-09-08 21:18:58 -0500

    CORBATalk graphical interface has been improved

--- MessageServerWindow.java
+++ MessageServerWindow.java
@@ -1,3 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
 import javax.swing.*;
 import java.awt.event.*;
 
@@ -11,7 +19,7 @@ public class MessageServerWindow extends JFrame implements ActionListener{
     JMenu mFile;
     JMenuItem miAddHost;
     JMenuItem miExit;
-    JButton jbSend;
+    JTextArea jtaSend;
     
     // CONSTRUCTORS
     public MessageServerWindow(){
@@ -31,9 +39,12 @@ public class MessageServerWindow extends JFrame implements ActionListener{
      */
     private void __setting_MessageServerWindow(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setBounds(0,0,400,400);
+        this.setBounds(0,0,445,380);
         this.setResizable(false);
         this.setLocationRelativeTo(null);
+        //this.setLayout(new BorderLayout());
+        this.setContentPane(new JLabel(new ImageIcon("images/corbatalk.png")));
+        //this.setLayout(new FlowLayout());
         mbMenu = new JMenuBar();
         this.setJMenuBar(mbMenu);
         mFile = new JMenu("File");
@@ -45,10 +56,14 @@ public class MessageServerWindow extends JFrame implements ActionListener{
         miExit.addActionListener(this);
         mFile.add(miExit);
         
-        jbSend = new JButton("Send");
-        jbSend.setSize(100,50);
-        jbSend.addActionListener(this);
-        this.add(jbSend);
+        jtaSend = new JTextArea();
+        //jtaSend.setBackground(new Color(0,0,0,0));
+        //jtaSend.setText("CORBATalk > ");
+        JScrollPane jspScroll = new JScrollPane(jtaSend);
+        jspScroll.setBounds(0,65,440,235);
+        jspScroll.setOpaque(false);
+        jspScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        this.add(jspScroll);
 
         this.setVisible(true);
     }
@@ -56,15 +71,13 @@ public class MessageServerWindow extends JFrame implements ActionListener{
     /** 
      * Method: ActionPerformed
      * Description: Takes an action corresponding to the MenuBar's window.
+     * @param e
      */
     @Override
     public void actionPerformed(ActionEvent e) {
         Object src = e.getSource();
         if(src == miAddHost){
             JOptionPane.showMessageDialog(null, "Run a new Console and follow instructions to start a new CORBATalk.");
-        }
-        else if(src == jbSend){
-            JOptionPane.showMessageDialog(null, "Mensaje enviado", "CORBATalk | Distributed Messages System", JOptionPane.INFORMATION_MESSAGE);
         }
         else if(src == miExit){
             System.exit(0);
@@ -73,9 +86,11 @@ public class MessageServerWindow extends JFrame implements ActionListener{
 
     /** 
     * Method: print
+    * Description: Receive the Server message and wrote it on screen.
+     * @param message
     */
     public void print(String message){
-    	jbSend.setText(message);
+    	jtaSend.append("CORBATalk > "+message+"\n");
     }
     
     public static void main(String[] args){
@@ -83,3 +98,4 @@ public class MessageServerWindow extends JFrame implements ActionListener{
         chat.setVisible(true);
     }
 }
+
